1697fcd2c46383dd7b406ea13546da99085aaa69
giculazar <ower.01@yandex.ru>
2021-10-13 13:21:40 +0300

    add variables from piano-notes.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React,{useState} from "react";
 import './App.css';
-import DO from "./piano-notes/noty-do.mp3"
-import RE from "./piano-notes/re.mp3"
-import MI from "./piano-notes/mi.mp3"
-import FA from "./piano-notes/fa.mp3"
-import SOL from "./piano-notes/sol.mp3"
-import LA from "./piano-notes/lja.mp3"
-import SI from "./piano-notes/si.mp3"
-import DOF from "./piano-notes/noty-do.mp3"
+import {DO,RE,MI,FA,DOF,SOL,SI,LA} from "./piano-notes/piano-notes"
 
 
 function App() {
---
+++ src/piano-notes/piano-notes.js
@@ -0,0 +1,11 @@
+import DO from "./piano-notes/noty-do.mp3"
+import RE from "./piano-notes/re.mp3"
+import MI from "./piano-notes/mi.mp3"
+import FA from "./piano-notes/fa.mp3"
+import SOL from "./piano-notes/sol.mp3"
+import LA from "./piano-notes/lja.mp3"
+import SI from "./piano-notes/si.mp3"
+import DOF from "./piano-notes/noty-do.mp3"
+
+
+export {DO,DOF,RE,MI,FA,SOL,SI,LA}
