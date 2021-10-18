ebf680f397f22105be13eaa9fb5ec2440bc88995
giculazar <ower.01@yandex.ru>
2021-10-18 12:44:12 +0300

    change functions to useReducer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React,{useState} from "react";
+import React,{useState,useReducer} from "react";
 import './App.css';
-import {DO,RE,MI,FA,DOF,SOL,SI,LA} from "./piano-notes/piano-notes"
+import {DO,RE,MI,FA,DOF,SOL,SI,LA} from "./piano-notes/piano-notes";
 
 
 function App() {
@@ -14,119 +14,117 @@ function App() {
     const audioSi = new Audio(SI)
     const audioDoF = new Audio(DOF)
 
+    const [noteDo, setNoteDo] = useState("");
+    const [noteRe, setNoteRe] = useState("");
+    const [noteMi, setNoteMi] = useState("");
+    const [noteFa, setNoteFa] = useState("");
+    const [noteSol, setNoteSol] = useState("");
+    const [noteLa, setNoteLa] = useState("");
+    const [noteSi, setNoteSi] = useState("");
+    const [noteDoF, setNoteDoF] = useState("");
 
-    const [noteDo,setNoteDo] = useState("")
-    const [noteRe,setNoteRe] = useState("")
-    const [noteMi,setNoteMi] = useState("")
-    const [noteFa,setNoteFa] = useState("")
-    const [noteSol,setNoteSol] = useState("")
-    const [noteLa,setNoteLa] = useState("")
-    const [noteSi,setNoteSi] = useState("")
-    const [noteDoF,setNoteDoF] = useState("")
-
-
-    const bindDo = () =>{
-        window.addEventListener('keydown', function(ev){
-            setNoteDo(ev.key)
-        if (noteDo === ev.key){
-            return audioDo.play()
+    const bindNotes = (state, action) => {
+        switch (action.type) {
+            case "bindDo":
+                window.addEventListener('keydown', function (ev) {
+                    setNoteDo(ev.key)
+                    if (noteDo === ev.key){
+                        return audioDo.play()
+                    }
+                });
+                break;
+            case "bindRe":
+                window.addEventListener('keydown', function (ev) {
+                    setNoteRe(ev.key)
+                    if (noteRe === ev.key) {
+                        return audioRe.play()
+                    }
+                });
+                break;
+            case "bindMi":
+                window.addEventListener('keydown', function (ev) {
+                    setNoteMi(ev.key)
+                    if (noteMi === ev.key) {
+                        return audioMi.play()
+                    }
+                });
+                break;
+            case "bindFa":
+                window.addEventListener('keydown', function (ev) {
+                    setNoteFa(ev.key)
+                    if (noteFa === ev.key) {
+                        return audioFa.play()
+                    }
+                });
+                break;
+            case "bindSol":
+                window.addEventListener('keydown', function (ev) {
+                    setNoteSol(ev.key)
+                    if (noteSol === ev.key) {
+                        return audioSol.play()
+                    }
+                });
+                break;
+            case "bindLa":
+                window.addEventListener('keydown', function (ev) {
+                    setNoteLa(ev.key)
+                    if (noteLa === ev.key) {
+                        return audioLa.play()
+                    }
+                });
+                break;
+            case "bindSi":
+                window.addEventListener('keydown', function (ev) {
+                    setNoteSi(ev.key)
+                    if (noteSi === ev.key) {
+                        return audioSi.play()
+                    }
+                });
+                break;
+            case "bindDoF":
+                window.addEventListener('keydown', function (ev) {
+                    setNoteDoF(ev.key)
+                    if (noteDoF === ev.key) {
+                        return audioDoF.play()
+                    }
+                });
+                break;
         }
-        })
-    };
 
-    const bindRe = () =>{
-        window.addEventListener('keydown', function(ev){
-            setNoteRe(ev.key)
-            if (noteRe === ev.key){
-                return audioRe.play()
-            }
-        })
     };
-
-    const bindMi = () =>{
-        window.addEventListener('keydown', function(ev){
-            setNoteMi(ev.key)
-            if (noteMi === ev.key){
-                return audioMi.play()
-            }
-        })
-    };
-
-    const bindFa = () =>{
-        window.addEventListener('keydown', function(ev){
-            setNoteFa(ev.key)
-            if (noteFa === ev.key){
-                return audioFa.play()
-            }
-        })
-    };
-
-    const bindSol = () =>{
-        window.addEventListener('keydown', function(ev){
-            setNoteSol(ev.key)
-            if (noteSol === ev.key){
-                return audioSol.play()
-            }
-        })
-    };
-
-    const bindLa = () =>{
-        window.addEventListener('keydown', function(ev){
-            setNoteLa(ev.key)
-            if (noteLa === ev.key){
-                return audioLa.play()
-            }
-        })
-    };
-
-    const bindSi = () =>{
-        window.addEventListener('keydown', function(ev){
-            setNoteSi(ev.key)
-            if (noteSi === ev.key){
-                return audioSi.play()
-            }
-        })
-    };
-
-    const bindDoF = () =>{
-        window.addEventListener('keydown', function(ev){
-            setNoteDoF(ev.key)
-            if (noteDoF === ev.key){
-                return audioDoF.play()
-            }
-        })
-    };
-
+    const initialState = {count: null};
+    const [state, dispatch] = useReducer(bindNotes,initialState);
 
     return (
-    <div className="App">
+            <div className="App">
+                <div className="piano">
+                    <button className="piano-notes" onClick={() => audioDo.play()}>DO</button>
+                    <button className="piano-notes" onClick={() => audioRe.play()}>RE</button>
+                    <button className="piano-notes" onClick={() => audioMi.play()}>MI</button>
+                    <button className="piano-notes" onClick={() => audioFa.play()}>FA</button>
+                    <button className="piano-notes" onClick={() => audioSol.play()}>SOL</button>
+                    <button className="piano-notes" onClick={() => audioLa.play()}>LA</button>
+                    <button className="piano-notes" onClick={() => audioSi.play()}>SI</button>
+                    <button className="piano-notes" onClick={() => audioDoF.play()}>DOF</button>
+                </div>
+                <div className="div-plus">
+                    Count: {state.count = null};
+                    <button className="plus plus-do" onClick={() => dispatch({type: 'bindDo'})} >+</button>
+                    <button className="plus"  onClick={() => dispatch({type: 'bindRe'})}>+</button>
+                    <button className="plus" onClick={() => dispatch({type: 'bindMi'})}>+</button>
+                    <button className="plus" onClick={() => dispatch({type: 'bindFa'})}>+</button>
+                    <button className="plus" onClick={() => dispatch({type: 'bindSol'})}>+</button>
+                    <button className="plus" onClick={() => dispatch({type: 'bindLa'})}>+</button>
+                    <button className="plus" onClick={() => dispatch({type: 'bindSi'})}>+</button>
+                    <button className="plus" onClick={() => dispatch({type: 'bindDoF'})}>+</button>
+                </div>
+                <div className="div-file">
+                    <button>File Upload</button>
+                </div>
 
-<div className="piano">
-  <button className="piano-notes" onClick={()=>audioDo.play()}>DO</button>
-    <button className="piano-notes" onClick={()=>audioRe.play()}>RE</button>
-  <button className="piano-notes" onClick={()=>audioMi.play()}>MI</button>
-  <button className="piano-notes" onClick={()=>audioFa.play()}>FA</button>
-  <button className="piano-notes" onClick={()=>audioSol.play()}>SOL</button>
-  <button className="piano-notes" onClick={()=>audioLa.play()}>LA</button>
-  <button className="piano-notes" onClick={()=>audioSi.play()}>SI</button>
-  <button className="piano-notes" onClick={()=>audioDoF.play()}>DOF</button>
-</div>
-        <div className="div-plus">
-            <button className="plus plus-do" onClick={bindDo}>+</button>
-            <button className="plus" onClick={bindRe}>+</button>
-            <button className="plus" onClick={bindMi}>+</button>
-            <button className="plus" onClick={bindFa}>+</button>
-            <button className="plus" onClick={bindSol}>+</button>
-            <button className="plus" onClick={bindLa}>+</button>
-            <button className="plus" onClick={bindSi}>+</button>
-            <button className="plus" onClick={bindDoF}>+</button>
-        </div>
-        <div className="div-file">
-            <button>File Upload</button>
-        </div>
+            </div>
+        )
 
-    </div>
-  );
 }
 
 export default App;
